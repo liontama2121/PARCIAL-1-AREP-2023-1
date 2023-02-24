db7ef35083e22f2aae3fe2a54f2310526689b74f
unknown <juan.molina-l@labinfo.is.escuelaing.edu.co>
2023-02-24 15:25:38 -0500

    parcial

---
+++ src/main/java/org/escuelaing.edu.co/Api.java
@@ -0,0 +1,7 @@
+package org.escuelaing.edu.co;
+
+public class Api {
+    private  String clase = "" ;
+
+
+}
---
+++ src/main/java/org/escuelaing.edu.co/HttpConnectionExample.java
@@ -0,0 +1,47 @@
+package org.escuelaing.edu.co;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.ProtocolException;
+import java.net.URL;
+
+public class HttpConnectionExample {
+
+    private static final String USER_AGENT = "Mozilla/5.0";
+
+
+    public static StringBuffer getChatGPT(String obClase) throws IOException {
+        String GET_URL = "";
+        URL obj = new URL(GET_URL);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+        con.setRequestMethod("GET");
+        con.setRequestProperty("User-Agent", USER_AGENT);
+
+        //The following invocation perform the connection implicitly before getting the code
+        StringBuffer response = new StringBuffer();
+        int responseCode = con.getResponseCode();
+        System.out.println("GET Response Code :: " + responseCode);
+
+        if (responseCode == HttpURLConnection.HTTP_OK) { // success
+            BufferedReader in = new BufferedReader(new InputStreamReader(
+                    con.getInputStream()));
+            String inputLine;
+
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+            // print result
+            System.out.println(response.toString());
+        } else {
+            System.out.println("GET request not worked");
+        }
+        System.out.println("GET DONE");
+        return  response;
+    }
+
+}
